9090926f2448532424d5d5d7e633c4dac2cbc2ac
Ben Feder <benfeder@Ben.localdomain>
2021-03-11 10:08:04 -0500

    All tests pass

--- nodict.py
+++ nodict.py
@@ -8,6 +8,7 @@ __author__ = 'Benjamin Feder'
 
 class Node:
     def __init__(self, key, value=None):
+        """Create key and value with a hash value for each Node"""
         self.hash = hash(key)
         self.key = key
         self.value = value
@@ -26,26 +27,45 @@ class Node:
 
 class NoDict:
     def __init__(self, num_buckets=10):
-        self.buckets = None
-        # Your code here
+        """Create list of bucket lists, number of times given"""
+        self.buckets = []
+        for _ in range(num_buckets):
+            self.buckets.append([])
+        self.num_buckets = num_buckets
 
     def __repr__(self):
         """Return a string representing the NoDict contents."""
         # We want to show all the buckets vertically
-        return '\n'.join([f'{self.__class__.__name__}.{i}:{bucket}' for i, bucket in enumerate(self.buckets)])
+        return '\n'.join([f'{self.__class__.__name__}.{i}:{bucket}' for i,
+                          bucket in enumerate(self.buckets)])
 
     def add(self, key, value):
-        # Your code here
-        return
+        """Create new node in given bucket, by looping through each bucket
+        to see if it belongs there, and delete it if it exists already
+        and replace it, if not there already, just add it to the end"""
+        new_node = Node(key, value)
+        bucket_index = new_node.hash % self.num_buckets
+        print(bucket_index)
+        print(len(self.buckets))
+        for node in self.buckets[bucket_index]:
+            if node == new_node:
+                self.buckets[bucket_index].remove(node)
+                break
+        self.buckets[bucket_index].append(new_node)
 
     def get(self, key):
-        # Your code here
-        return
+        """Just like adding a new node, except it finds it and returns it"""
+        node_to_find = Node(key)
+        bucket_index = node_to_find.hash % self.num_buckets
+        for node in self.buckets[bucket_index]:
+            if node == node_to_find:
+                return node.value
+        raise KeyError(f'{key} not found')
 
     def __getitem__(self, key):
-        # Your code here
-        return
+        """Call the get method"""
+        return self.get(key)
 
     def __setitem__(self, key, value):
-        # Your code here
-        return
+        """Call the add method"""
+        self.add(key, value)
